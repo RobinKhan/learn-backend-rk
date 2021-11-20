e6714696b5abf058d9a31f879f4d9a6f11227834
RobinKhan <arahaman235@gmail.com>
2021-11-20 20:51:40 +0600

    1st Commit-20Nov21

---
+++ learn-backend-rk/src/main/java/net/com/backendspringboot/rk/LearnBackendRkApplication.java
@@ -0,0 +1,15 @@
+package net.com.backendspringboot.rk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnBackendRkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnBackendRkApplication.class, args);
+		
+		System.out.println("Learn App just started");
+	}
+
+}
---
+++ learn-backend-rk/src/test/java/net/com/backendspringboot/rk/LearnBackendRkApplicationTests.java
@@ -0,0 +1,13 @@
+package net.com.backendspringboot.rk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnBackendRkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
